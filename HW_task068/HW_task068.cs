2fa5745802125dabc8e50c5f8724cd4167614552
krenartem <krenartem@yandex.ru>
2022-09-15 14:48:04 +0300

    added homework task 068

---
+++ HW_task068/HW_task068.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу вычисления функции Аккермана
+// с помощью рекурсии. Даны два неотрицательных числа m и n.
+int CalculateAkkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0)
+    {
+        return CalculateAkkerman(m - 1, 1);
+    }
+    else
+    {
+        return CalculateAkkerman(m - 1, CalculateAkkerman(m, n - 1));
+    }
+}
+
+Console.Clear();
+Console.Write("Введите неотрицательное значение M: ");
+int M = int.Parse(Console.ReadLine());
+Console.Write("Введите неотрицательное значение N: ");
+int N = int.Parse(Console.ReadLine());
+Console.Write("A(m,n) = ");
+Console.WriteLine($"{CalculateAkkerman(M, N)}");
