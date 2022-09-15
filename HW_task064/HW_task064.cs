6b8add3a5873812f763ff2a97065916daa2a7a9d
krenartem <krenartem@yandex.ru>
2022-09-15 12:45:43 +0300

    added homework task 064

---
+++ HW_task064/HW_task064.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значения M и N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от M до N.
+int ShowNaturalNumbers(int m, int n)
+{
+    if (m == n)
+    {
+        return m;
+    }
+    else
+    {
+        Console.Write(m + ", ");
+        return ShowNaturalNumbers(m + 1, n);
+    }
+}
+
+Console.Clear();
+Console.Write("Введите минимальное значение M: ");
+int min = int.Parse(Console.ReadLine());
+Console.Write("Введите максимальное значение N: ");
+int max = int.Parse(Console.ReadLine());
+Console.WriteLine("Натуральные числа в промежутке от M до N:");
+Console.WriteLine($"{ShowNaturalNumbers(min,max)}");
